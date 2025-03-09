f55c448827221428718569b410600842bcda2b59
Nseigou <nagano_1si@yahoo.co.jp>
2025-03-09 18:33:59 +0900

    developing-header

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,9 +1,12 @@
 // src/app/page.tsx
 import HorseTable from '@/components/HorseTable';
+import Head from 'next/head';
+import Header from '@/components/layout/Header';
 
 export default function Home() {
   return (
     <main className="min-h-screen p-4 bg-gray-50">
+      <Header />
       <HorseTable />
     </main>
   );
