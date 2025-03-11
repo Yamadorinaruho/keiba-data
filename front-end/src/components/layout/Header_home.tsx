4dcabca04518d89b5f3e86abc302dad89023c59f
Nseigou <nagano_1si@yahoo.co.jp>
2025-03-11 18:41:11 +0900

    feat:3DModel

--- front-end/src/components/layout/Header_home.tsx
+++ front-end/src/components/layout/Header_home.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 export default function Header_home() {
 
     return (
-        <header>
+        <header className='fixed top-0 left-0 right-0 z-50'>
             <nav className="flex justify-between items-center px-10 " style={{ backgroundColor: "#FFD700" }}>
                 <div>
                     <Link href="/" className="flex items-center">
