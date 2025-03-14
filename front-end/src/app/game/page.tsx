9665bed48109f68631739b01547b33a7cbe4ddfe
Nseigou <nagano_1si@yahoo.co.jp>
2025-03-14 20:10:42 +0900

    developing result alt

--- front-end/src/app/game/page.tsx
+++ front-end/src/app/game/page.tsx
@@ -2,14 +2,12 @@
 import HorseTable from '@/components/HorseTable';
 import Head from 'next/head';
 import Header_game from '@/components/layout/Header_game';
-import Result from '@/components/Result';
 
 export default function Game() {
   return (
     <main className="min-h-screen p-4 bg-gray-50 pt-15">
       <Header_game />
-      {/* <HorseTable /> */}
-      <Result winner="AI" />
+      <HorseTable />
     </main>
   );
 }
